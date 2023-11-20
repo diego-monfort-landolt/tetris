02fc1de2a137eec888b61e8dd5cf86791f69d3f9
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-11-20 20:57:53 +0100

    add rotated an resolve bug from rotated on the wall

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import './style.css'
+import "./style.css";
 
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
-const BLOCK_SIZE = 20
-const BOARD_WIDTH = 14
-const BOARD_HEIGHT = 30
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 14;
+const BOARD_HEIGHT = 30;
 
-canvas.width = BLOCK_SIZE * BOARD_WIDTH
-canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+canvas.width = BLOCK_SIZE * BOARD_WIDTH;
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
 
-context.scale(BLOCK_SIZE, BLOCK_SIZE)
+context.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 // 3.board
 
@@ -46,9 +46,8 @@ const board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1]
-
-]
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+];
 
 // 4. pieza player
 
@@ -56,143 +55,158 @@ const piece = {
   position: { x: 5, y: 5 },
   shape: [
     [1, 1],
-    [1, 1]
-  ]
-}
+    [1, 1],
+  ],
+};
 
 // .random pieces
 const PIECES = [
   [
     [1, 1],
-    [1, 1]
+    [1, 1],
   ],
-  [
-    [1, 1, 1, 1]
-  ],
+  [[1, 1, 1, 1]],
   [
     [0, 1, 0],
-    [1, 1, 1]
+    [1, 1, 1],
   ],
   [
     [1, 1, 0],
-    [0, 1, 1]
+    [0, 1, 1],
   ],
   [
     [1, 0],
     [1, 0],
-    [1, 1]
-  ]
-]
+    [1, 1],
+  ],
+];
 
 // 2.game loop
-let dropCounter = 0
-let lastTime = 0
-function update (time = 0) {
-  const deltaTime = time - lastTime
-  lastTime = time
+let dropCounter = 0;
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
 
-  dropCounter += deltaTime
+  dropCounter += deltaTime;
   if (dropCounter > 1000) {
-    piece.position.y++
-    dropCounter = 0
+    piece.position.y++;
+    dropCounter = 0;
 
     if (checkCollision()) {
-      piece.position.y--
-      solidifyPiece()
-      removeRows()
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
     }
   }
 
-  draw()
-  window.requestAnimationFrame(update)
+  draw();
+  window.requestAnimationFrame(update);
 }
 
-function draw () {
-  context.fillStyle = '#000'
-  context.fillRect(0, 0, canvas.width, canvas.height)
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
   board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        context.fillStyle = 'blue'
-        context.fillRect(x, y, 1, 1)
+        context.fillStyle = "blue";
+        context.fillRect(x, y, 1, 1);
       }
-    })
-  })
+    });
+  });
 
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value) {
-        context.fillStyle = 'red'
-        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+        context.fillStyle = "red";
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
       }
-    })
-  })
+    });
+  });
 }
-document.addEventListener('keydown', event => {
-  if (event.key === 'ArrowLeft') {
-    piece.position.x--
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
     if (checkCollision()) {
-      piece.position.x++
+      piece.position.x++;
     }
   }
-  if (event.key === 'ArrowRight') {
-    piece.position.x++
+  if (event.key === "ArrowRight") {
+    piece.position.x++;
     if (checkCollision()) {
-      piece.position.x--
+      piece.position.x--;
     }
   }
-  if (event.key === 'ArrowDown') {
-    piece.position.y++
+  if (event.key === "ArrowDown") {
+    piece.position.y++;
     if (checkCollision()) {
-      piece.position.y--
-      solidifyPiece()
-      removeRows()
+      piece.position.y--;
+      solidifyPiece();
+      removeRows();
     }
   }
-})
-function checkCollision () {
+
+  if (event.key === "ArrowUp") {
+    const rotated = [];
+
+    for (let i = 0; i < piece.shape[0].length; i++) {
+      const row = [];
+
+      for (let j = piece.shape.length - 1; j >= 0; j--) {
+        row.push(piece.shape[j][i]);
+      }
+      rotated.push(row);
+    }
+    const previousShape = piece.shape
+    piece.shape = rotated
+    if (checkCollision()) {
+      piece.shape = previousShape
+    }
+  }
+});
+function checkCollision() {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
       return (
-        value !== 0 &&
-        board[y + piece.position.y]?.[x + piece.position.x] !== 0
-      )
-    })
-  })
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      );
+    });
+  });
 }
-function solidifyPiece () {
+function solidifyPiece() {
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        board[y + piece.position.y][x + piece.position.x] = 1
+        board[y + piece.position.y][x + piece.position.x] = 1;
       }
-    })
-  })
+    });
+  });
   // reset position
-  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2)
-  piece.position.y = 0
+  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2);
+  piece.position.y = 0;
   // get random shape
-  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)];
   // Game over
   if (checkCollision()) {
-    window.alert('¡¡ Game Over !!')
-    board.forEach((row) => row.fill(0))
+    window.alert("¡¡ Game Over !!");
+    board.forEach((row) => row.fill(0));
   }
 }
 
-function removeRows () {
-  const rowsToRemove = []
+function removeRows() {
+  const rowsToRemove = [];
   board.forEach((row, y) => {
-    if (row.every(value => value === 1)) {
-      rowsToRemove.push(y)
+    if (row.every((value) => value === 1)) {
+      rowsToRemove.push(y);
     }
-  })
-  rowsToRemove.forEach(y => {
-    board.splice(y, 1)
-    const newRow = Array(BOARD_WIDTH).fill(0)
-    board.unshift(newRow)
-  })
+  });
+  rowsToRemove.forEach((y) => {
+    board.splice(y, 1);
+    const newRow = Array(BOARD_WIDTH).fill(0);
+    board.unshift(newRow);
+  });
 }
 
-update()
+update();
